004b3c6f4244f81dab1ae8e35c66bbe25fb2f7bb
adontoo <1056651908@qq.com>
2025-06-21 17:30:25 +0800

    Switch to AIDL bootctrl and fix flash offical rom

--- device.mk
+++ device.mk
@@ -13,22 +13,24 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 # Configure Virtual A/B
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
-# Configure virtual_ab compression.mk
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
+# Configure virtual_ab_ota compression_with_xor.mkAdd commentMore actions
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression_with_xor.mk)
 
 # Configure emulated_storage.mk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
-# Configure twrp common.mk
+# Configure twrp config common.mk
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # API
 BOARD_SHIPPING_API_LEVEL := 34
 PRODUCT_SHIPPING_API_LEVEL := 34
 PRODUCT_TARGET_VNDK_VERSION := 34
+
+#Encryption
 PRODUCT_PACKAGES += \
-    bootctrl.meizu_sm8550.recovery \
-    android.hardware.boot@1.2-impl-qti.recovery
+    qcom_decrypt \
+    qcom_decrypt_fbe
 
 # Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
@@ -39,6 +41,3 @@ PRODUCT_PROPERTY_OVERRIDES += persist.sys.fuse.passthrough.enable=true
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(DEVICE_PATH)
-
-TWRP_REQUIRED_MODULES += \
-    flyme_prebuilt
